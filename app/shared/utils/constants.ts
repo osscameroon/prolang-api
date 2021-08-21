96cd39a7edf15f7eb908ab2f05ef1f9093c48d1c
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-08-21 15:36:42 +0200

    feat: create endpoints for group of years

--- app/shared/utils/constants.ts
+++ app/shared/utils/constants.ts
@@ -2,3 +2,7 @@ export const YEAR_GROUP_NOT_CREATED_MESSAGE = 'Year group for not listed languag
 export const WIKIPEDIA_URL = 'https://en.wikipedia.org';
 export const YEAR_GROUP_NOT_LISTED = 'Not Listed';
 export const PROGRAMMING_PAGE_URL = 'https://en.wikipedia.org/wiki/Timeline_of_programming_languages';
+export const PATH_NOT_FOUND = 'Path not found';
+export const RECORD_NOT_FOUND_MESSAGE = (model: string, id: string) => {
+  return `No ${model} found with id: ${id}`;
+};
