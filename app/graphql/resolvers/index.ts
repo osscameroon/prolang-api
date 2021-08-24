c5dc14d6fb42dfc2b6a087598d2f1578084adc5b
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-08-24 17:36:50 +0200

    feat: create queries for year groups

--- app/graphql/resolvers/index.ts
+++ app/graphql/resolvers/index.ts
@@ -1,8 +1,12 @@
 import { hello } from './queries/hello';
+import { allYearGroups } from './queries/allYearGroups';
+import { oneYearGroup } from './queries/oneYearGroup';
 
 const resolvers = {
   Query: {
+    allYearGroups,
     hello,
+    oneYearGroup,
   },
 };
 
---
+++ app/graphql/resolvers/queries/oneYearGroup.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import { ApolloError } from 'apollo-server-express';
+
+import { QueryResolvers } from '../../types/types';
+import yearGroupService from '../../../domain/services/yearGroup.service';
+import { transformYearGroupResponse } from '../../../domain/responses/yearGroup.response';
+import { YearGroupResponse } from '../../../shared/types/responses';
+import { RECORD_NOT_FOUND_CODE, RECORD_NOT_FOUND_MESSAGE } from '../../../shared/utils/constants';
+
+export const oneYearGroup: QueryResolvers['oneYearGroup'] = async (_context, args) => {
+  const { idOrName } = args;
+  const isObjectId = mongoose.Types.ObjectId.isValid(idOrName);
+
+  const item = await (isObjectId ? yearGroupService.findById(idOrName) : yearGroupService.findByName(idOrName));
+
+  if (!item) {
+    throw new ApolloError(RECORD_NOT_FOUND_MESSAGE('YearGroup', idOrName), RECORD_NOT_FOUND_CODE);
+  }
+
+  return transformYearGroupResponse(item) as YearGroupResponse;
+};
