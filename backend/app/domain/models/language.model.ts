741a4a6a2bd03df0d099e0ccc5a8d886827a3a23
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-08-26 10:32:56 +0200

    feat: change the project structure

---
+++ backend/app/domain/models/language.model.ts
@@ -0,0 +1,60 @@
+import mongoose, { Model, Schema } from 'mongoose';
+
+import { LanguageDocument } from '../../shared/types/models';
+
+const languageSchema = new Schema(
+  {
+    authors: {
+      index: true,
+      ref: 'AuthorModel',
+      type: [Schema.Types.ObjectId],
+    },
+    company: Schema.Types.String,
+    link: Schema.Types.String,
+    listed: {
+      default: false,
+      required: true,
+      type: Schema.Types.Boolean,
+    },
+    name: {
+      index: true,
+      required: true,
+      type: Schema.Types.String,
+    },
+    nameExtra: {
+      link: Schema.Types.String,
+      name: Schema.Types.String,
+    },
+    predecessors: {
+      index: true,
+      ref: 'LanguageModel',
+      type: [Schema.Types.ObjectId],
+    },
+    yearConfirmed: {
+      default: true,
+      required: true,
+      type: Schema.Types.Boolean,
+    },
+    yearGroup: {
+      index: true,
+      ref: 'YearGroupModel',
+      required: true,
+      type: Schema.Types.ObjectId,
+    },
+    years: {
+      index: true,
+      required: true,
+      type: [Schema.Types.Number],
+    },
+  },
+  {
+    collection: 'languages',
+    timestamps: true,
+  },
+);
+
+languageSchema.index({ name: 1, years: 1 }, { unique: true });
+
+const LanguageModel: Model<LanguageDocument> = mongoose.model<LanguageDocument>('LanguageModel', languageSchema);
+
+export { LanguageModel };
---
+++ backend/app/rest/server.ts
@@ -0,0 +1,47 @@
+import express, { Application } from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import { commonRoute } from './routes/common.route';
+import { yearGroupRoute } from './routes/yearGroup.route';
+import { notFoundMiddleware } from '../shared/core/middleware/notFound';
+import { authorRoute } from './routes/author.route';
+import { languageRoute } from './routes/language.route';
+import { logRequestMiddleware } from '../shared/core/middleware/logRequest';
+import { rateLimiterMiddleware } from '../shared/core/middleware/rateLimiter';
+import { Sentry } from '../shared/core/sentry';
+import { errorHandlerMiddleware } from '../shared/core/middleware/errorMiddleware';
+import { SENTRY_ENABLED } from '../shared/core/config';
+
+export const setupRestEndpoints = (app: Application) => {
+  const router = express.Router();
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(cors());
+
+  app.set('trust proxy', true);
+
+  if (SENTRY_ENABLED) {
+    app.use(Sentry.Handlers.requestHandler());
+  }
+  app.use(rateLimiterMiddleware);
+
+  app.use(logRequestMiddleware);
+
+  app.use('/', router);
+  app.use('/', commonRoute());
+  app.use('/api', yearGroupRoute());
+  app.use('/api', authorRoute());
+  app.use('/api', languageRoute());
+
+  app.use(express.static(path.join(__dirname, '../../public')));
+
+  app.use(notFoundMiddleware);
+
+  if (SENTRY_ENABLED) {
+    app.use(Sentry.Handlers.errorHandler());
+  }
+
+  app.use(errorHandlerMiddleware);
+};
