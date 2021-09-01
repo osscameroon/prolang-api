8dc043157669e6fd28b000bbab2ea6efcd4443eb
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-09-01 22:44:54 +0200

    feat: add year group filter on languages query

--- backend/app/graphql/resolvers/queries/languages.ts
+++ backend/app/graphql/resolvers/queries/languages.ts
@@ -1,9 +1,23 @@
 import { QueryResolvers } from '../../types/types';
 import languageService from '../../../domain/services/language.service';
 import { PAGINATION_LIMIT } from '../../../shared/core/config';
+import yearGroupService from '../../../domain/services/yearGroup.service';
 
 export const languages: QueryResolvers['languages'] = async (_context, args) => {
-  const { page, search } = args;
+  const { page, search, yearGroupName } = args;
 
-  return await languageService.findPaginate(page || 1, PAGINATION_LIMIT, search || undefined);
+  let yearGroup = null;
+
+  if (yearGroupName) {
+    yearGroup = await yearGroupService.findByName(yearGroupName);
+  }
+
+  return languageService.findPaginate(
+    page || 1,
+    PAGINATION_LIMIT,
+    search || undefined,
+    undefined,
+    undefined,
+    yearGroup?._id,
+  );
 };
