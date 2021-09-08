49506c82519d847bd96827148b2ba8bab57b30c3
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-09-08 05:54:07 +0200

    feat: serve api documentation from the backend

--- backend/app/rest/routes/common.route.ts
+++ backend/app/rest/routes/common.route.ts
@@ -1,10 +1,11 @@
+import path from 'path';
 import { Router, Request, Response } from 'express';
 
 const commonRoute = () => {
   const router = Router();
 
-  router.get('/', (_req: Request, res: Response) => {
-    res.json({ message: 'Hello from programming languages API' });
+  router.get('/api', (_req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../docs/index.html'));
   });
 
   return router;
