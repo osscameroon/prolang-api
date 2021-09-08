ce62a7376a54fe87aa16b03459c741f536f475d1
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-09-08 06:09:07 +0200

    feat: load api documentation in the frontend

--- frontend/pages/documentation.tsx
+++ frontend/pages/documentation.tsx
@@ -1,7 +1,10 @@
 const Documentation = () => {
   return (
     <div className="w-full h-full overflow-y-auto" >
-      <iframe className="w-full h-screen overflow-y-auto" src="http://127.0.0.1:8080" />
+      <iframe 
+        className="w-full h-screen overflow-y-auto"
+        src={process.env.NEXT_PUBLIC_API_DOC_URL}
+      />
     </div>
   );
 };
