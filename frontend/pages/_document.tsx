0211a42baf92b9980bed1b6e3e3b66cecdd974a7
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-09-26 14:40:43 +0200

    feat: configure seo for pages

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { GA_TRACKING_ID } from '@utils/gtag';
 
 class MyDocument extends Document {
   render() {
@@ -6,6 +7,23 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+          {process.env.NODE_ENV === 'production' && Boolean(GA_TRACKING_ID) && (
+            <>
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </>
+          )}
         </Head>
         <body>
           <Main />
