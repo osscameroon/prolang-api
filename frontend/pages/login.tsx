4a3871b553b76dd3f214f22b056256d1c51ced2f
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-09-26 16:09:09 +0200

    feat: redesign the login page

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -5,7 +5,6 @@ import * as yup from 'yup';
 import { toast } from 'react-toastify';
 
 import PublicLayout from '@components/layout/public/public-layout';
-import { Img } from '@components/common/img';
 import { BAD_LOGIN_MESSAGE, FORM_ERRORS } from '@utils/constants';
 import { FormInput } from '@components/common/form-input';
 import { Button } from '@components/common/button';
@@ -47,47 +46,31 @@ export default function Home() {
 
   return (
     <PublicLayout>
-      <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
-        <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
-          <div className="flex flex-col overflow-y-auto md:flex-row">
-            <div className="h-32 md:h-auto md:w-1/2">
-              <Img
-                aria-hidden="true"
-                className="object-cover w-full h-full dark:hidden"
-                src="/assets/img/login-office.jpeg"
-                alt="Office"
-              />
-              <Img
-                aria-hidden="true"
-                className="hidden object-cover w-full h-full dark:block"
-                src="/assets/img/login-office-dark.jpeg"
-                alt="Office"
-              />
-            </div>
-            <div className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
-              <div className="w-full">
-                <h1 className="mb-8 text-xl font-semibold text-gray-700 dark:text-gray-200">Log into Wedding</h1>
-                <FormProvider {...formMethods}>
-                  <form onSubmit={formMethods.handleSubmit(handleLogin)}>
-                    <FormInput
-                      label="Email address"
-                      name="email"
-                      type="email"
-                      placeholder="jane.doe@email.com"
-                      isRequired
-                    />
-                    <FormInput
-                      label="Password"
-                      name="password"
-                      type="password"
-                      isRequired
-                    />
-                    <Button text="Log in" className="w-full justify-center" loading={loginMutation.isLoading} />
-                  </form>
-                </FormProvider>
+      <div className="flex items-center min-h-[calc(100vh-130px)] p-6 bg-gray-50 dark:bg-gray-900">
+        <div className="h-full w-1/4 mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
+          <div className="flex items-center justify-center p-6 sm:p-12 md:w-full">
+            <div className="w-full">
+              <h1 className="mb-8 text-xl font-semibold text-gray-700 dark:text-gray-200">Log into Prolang</h1>
+              <FormProvider {...formMethods}>
+                <form onSubmit={formMethods.handleSubmit(handleLogin)}>
+                  <FormInput
+                    label="Email address"
+                    name="email"
+                    type="email"
+                    placeholder="jane.doe@email.com"
+                    isRequired
+                  />
+                  <FormInput
+                    label="Password"
+                    name="password"
+                    type="password"
+                    isRequired
+                  />
+                  <Button text="Log in" className="w-full justify-center mt-12" loading={loginMutation.isLoading} />
+                </form>
+              </FormProvider>
 
-                <hr className="my-8" />
-              </div>
+              <hr className="my-8" />
             </div>
           </div>
         </div>
--- frontend/utils/colors.ts
+++ frontend/utils/colors.ts
@@ -1,7 +1,7 @@
 import { BgColorVariants } from '@typings/common';
 
 const backgroundColors: Record<BgColorVariants, string> = {
-  primary: 'bg-purple-600 active:bg-purple-600 hover:bg-purple-700 focus:shadow-outline-purple',
+  primary: 'bg-blue-600 active:bg-blue-600 hover:bg-blue-700 focus:shadow-outline-blue',
   red: 'bg-red-600 hover:bg-red-700 focus:shadow-outline-red',
   whiteGray: 'bg-white text-gray-700 hover:bg-gray-5 border-gray-300 focus:shadow-outline-gray',
 };
