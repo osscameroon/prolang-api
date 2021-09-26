13b73b3143110ea14ac64572051639a9a8b7a06f
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-09-26 15:03:33 +0200

    feat: change color of the application

--- frontend/utils/gtag.ts
+++ frontend/utils/gtag.ts
@@ -2,6 +2,10 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageView = (url: string) => {
+  if (!GA_TRACKING_ID) {
+    return;
+  }
+
   // @ts-ignore
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
