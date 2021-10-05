0f9a684b5799f5facc940dea8fe052ad3f45f929
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-10-06 00:38:29 +0200

    feat: add arguments queries in requests log

--- backend/app/graphql/utils/requestPlugin.ts
+++ backend/app/graphql/utils/requestPlugin.ts
@@ -5,6 +5,17 @@ import requestLogService from '../../domain/services/requestLog.service';
 import { getDurationInMilliseconds } from '../../shared/utils/request';
 import { GraphQLRequestContext } from 'apollo-server-types/src/index';
 import { AppContext } from '../types/common';
+import * as querystring from 'querystring';
+
+const generateEndpointURL = (operation: string, args: any[][]) => {
+  if (args.length === 0) {
+    return operation;
+  }
+  const argsObject = Object.fromEntries(args);
+  const queryStringArgs = querystring.stringify(argsObject);
+
+  return `${operation}?${queryStringArgs}`;
+};
 
 const requestPlugin: ApolloServerPlugin = {
   // @ts-ignore
@@ -19,22 +30,23 @@ const requestPlugin: ApolloServerPlugin = {
           return;
         }
 
-        const obj = parse(query);
-
-        // TODO get the variables
-        const operationDefinition = obj.definitions[0] as OperationDefinitionNode;
-        const selection = operationDefinition.selectionSet.selections[0] as FieldNode;
-
-        const input: CreateRequestLogInput = {
-          duration: null,
-          endpoint: selection.name.value,
-          ipAddress: context.context.ip,
-          statusCode: 200,
-          type: RequestTypeEnum.graphql,
-        };
-
         context.context.xRes.on('finish', async () => {
           if (context.request.operationName !== 'IntrospectionQuery') {
+            const obj = parse(query);
+
+            const operationDefinition = obj.definitions[0] as OperationDefinitionNode;
+            const selection = operationDefinition.selectionSet.selections[0] as FieldNode;
+            // @ts-ignore
+            const args = selection.arguments?.map((arg) => [arg.name.value, arg.value.value]) || [];
+
+            const input: CreateRequestLogInput = {
+              duration: null,
+              endpoint: generateEndpointURL(selection.name.value, args),
+              ipAddress: context.context.ip,
+              statusCode: 200,
+              type: RequestTypeEnum.graphql,
+            };
+
             input.duration = getDurationInMilliseconds(context.context.reqStartTime);
 
             await requestLogService.create(input);
