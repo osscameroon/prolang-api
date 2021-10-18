0b6a5ef6cc13c6b748611529cfc5cfe2a22db895
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-10-18 23:23:58 +0200

    feat: add 404 page

---
+++ frontend/pages/404.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import { ExclamationCircleIcon } from '@heroicons/react/outline';
+
+const PageNotFound = () => {
+  return (
+    <div className="container px-6 mx-auto h-full grid">
+      <div className="px-12 py-12 w-1/2 mx-auto min-h-1/2 my-auto bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <div className="flex justify-center text-2xl mb-4 text-red-600">
+          <ExclamationCircleIcon className="w-16 h-16" />
+        </div>
+        <h2 className="text-center font-bold text-3xl">Oops! The page not found</h2>
+
+        <p className="text-center text-xl my-10">It looks like the page you are looking for doesn't exists.</p>
+
+        <div className="flex justify-center mt-8">
+          <Link href="/">
+            <a className="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white bg-red-600 border border-transparent rounded-lg focus:outline-none">
+                Go to the home page
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
