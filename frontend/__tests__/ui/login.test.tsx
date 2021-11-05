6988def205f117b83c5120d942ca2e0508c12621
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-11-05 23:51:36 +0100

    test: write test for login form with invalide email

--- frontend/__tests__/ui/login.test.tsx
+++ frontend/__tests__/ui/login.test.tsx
@@ -1,10 +1,10 @@
 import React, {PropsWithChildren} from 'react';
-import { render, fireEvent, cleanup, screen } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from "react-query";
+import { render, fireEvent, cleanup, screen } from '@testing-library/react';
+import {act} from "react-dom/test-utils";
 import {rest} from 'msw'
 import {setupServer} from 'msw/node'
 import Login from "../../pages/login";
-import {act} from "react-dom/test-utils";
 
 const queryClient = new QueryClient();
 
@@ -26,7 +26,7 @@ const server = setupServer(
     }),
 )
 
-describe('Login page', () => {
+describe.only('Login page', () => {
   beforeAll(() => server.listen());
 
   afterEach(() => {
@@ -36,10 +36,11 @@ describe('Login page', () => {
 
   afterAll(() => server.close());
 
-  it.only('should display errors when inputs are empty', async () => {
+  it('should display errors when inputs are empty', async () => {
     render(<Login />, { wrapper });
 
     // const loginForm = screen.getByTestId('login-form');
+    // screen.debug(loginForm);
 
     await act(async () => {
       fireEvent.click(screen.getByTestId('btn-submit'));
@@ -47,9 +48,44 @@ describe('Login page', () => {
 
     const inputEmail = screen.getByRole('textbox', { name: /Email address*/i });
 
-    expect(inputEmail.textContent).toEqual('');
+    // @ts-ignore
+    expect(inputEmail.value).toEqual('');
 
     const errorLabels = screen.getAllByText('This field is required.', { exact: true });
     expect(errorLabels).toHaveLength(2);
+  });
+
+  it('should display errors when email is invalid', async () => {
+    const badEmailValue = 'this-is-bad-email';
+    const validPasswordValue = 'Passw04D$';
+
+    const  { container } = render(<Login />, { wrapper });
+
+    const inputEmail = screen.getByRole('textbox', { name: /Email address*/i });
+    const inputPassword = container.querySelector("input[name='password']");
+
+    await act(async () => {
+      fireEvent.input(inputEmail, {
+        target: {
+          value: badEmailValue
+        }
+      });
+      // @ts-ignore
+      fireEvent.input(inputPassword, {
+        target: {
+          value: validPasswordValue
+        }
+      });
+
+      fireEvent.click(screen.getByTestId('btn-submit'));
+    })
+
+    // @ts-ignore
+    expect(inputEmail.value).toEqual(badEmailValue);
+    // @ts-ignore
+    expect(inputPassword.value).toEqual(validPasswordValue);
+
+    const errorLabels = screen.getAllByText('The email address is invalid.', { exact: true });
+    expect(errorLabels).toHaveLength(1);
   });
 });
