c79fb019608ce351dd93e24f7006e6d314635e4a
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-11-26 22:54:19 +0100

    ci: finalize github action for the backend

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,15 @@ cd prolang
 
 docker pull tericcabrel/prolang-api:latest
 
+API_CONTAINER_NAME=prolang-api
+if [ "$(docker ps -qa -f name=$API_CONTAINER_NAME)" ]; then
+    if [ "$(docker ps -q -f name=$API_CONTAINER_NAME)" ]; then
+        echo "[API] Container is running -> stopping it..."
+        docker stop $API_CONTAINER_NAME;
+    fi
+    docker rm $API_CONTAINER_NAME;
+fi
+
 docker run -d -p 5700:5700  -v $(pwd)/logs:/app/logs --name prolang-api --rm --env-file .env tericcabrel/prolang-api:latest
 
 exit
