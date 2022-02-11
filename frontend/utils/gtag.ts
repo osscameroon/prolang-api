9f4f7cc6b4a0be284a74e8977dd966e069c89e07
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-02-11 22:51:29 +0100

    feat(analytics): filter analytic for private area

--- frontend/utils/gtag.ts
+++ frontend/utils/gtag.ts
@@ -1,8 +1,18 @@
+import { isProduction } from '@utils/common';
+
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+const TRACKABLE_PATH: string[] = ['/', '/documentation', '/playground', '/error', '/login'];
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageView = (url: string) => {
   if (!GA_TRACKING_ID) {
+    return;
+  }
+
+  const canLogAnalytic = isProduction() && TRACKABLE_PATH.includes(url);
+
+  if (!canLogAnalytic) {
     return;
   }
 
