80a851e1afbc65fef04f87cf0e738b5d4f580ecf
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-02-11 22:51:29 +0100

    feat(analytics): filter analytic for private area

--- frontend/utils/common.ts
+++ frontend/utils/common.ts
@@ -1,2 +1,4 @@
 export const padZero = (value: number) => (value < 10 ? `0${value}` : value.toString());
 export const noop = () => {};
+
+export const isProduction = () => process.env.NODE_ENV === 'production';
